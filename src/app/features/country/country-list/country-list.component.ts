011e6e94874f09834c89c82f1bc2239d5699ece4
iamprashant29 <prashantiyengar@Prashants-MacBook-Air.local>
2025-04-21 01:56:35 +0530

    Displayed countries list

--- src/app/features/country/country-list/country-list.component.ts
+++ src/app/features/country/country-list/country-list.component.ts
@@ -1,13 +1,16 @@
 import {Component, OnInit} from '@angular/core';
 import {SharedModule} from '../../../shared/shared.module';
-import {CountryService} from "../country.service";
-import {FormControl, FormGroup} from "@angular/forms";
+import {CountryService} from '../country.service';
+import {FormControl, FormGroup} from '@angular/forms';
+import {MatInput} from '@angular/material/input';
+import {MatCard, MatCardContent, MatCardImage} from '@angular/material/card';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-country-list',
   templateUrl: './country-list.component.html',
   styleUrl: 'country-list.component.scss',
-  imports: [SharedModule],
+  imports: [SharedModule, MatInput, MatCard, MatCardContent, MatCardImage, NgOptimizedImage],
 })
 export class CountryListComponent implements OnInit {
   countriesList: any[] = [];
@@ -37,7 +40,13 @@ export class CountryListComponent implements OnInit {
    **/
   private getCountriesList() {
     this.countryService.getCountries().subscribe((response) => {
-      this.countriesList = response;
+      this.countriesList = response.sort((a: any, b: any) => {
+        if (a?.name?.common < b?.name?.common)
+          return -1;
+        if (a?.name?.common > b?.name?.common)
+          return 1;
+        return 0;
+      });
       console.log(this.countriesList);
     });
   }
