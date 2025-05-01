c6be1fa911835c3b7f2e810e072ba3e3b8199534
iamprashant29 <prashantiyengar@Prashants-MacBook-Air.local>
2025-05-02 00:28:29 +0530

    Handled API error via interceptor and display toaster on error

---
+++ src/app/shared/error.interceptor.ts
@@ -0,0 +1,17 @@
+import {HttpErrorResponse, HttpEvent, HttpHandlerFn, HttpRequest} from '@angular/common/http';
+import {catchError, Observable} from 'rxjs';
+import {inject} from '@angular/core';
+import {ErrorService} from './error.service';
+
+export function errorInterceptor(req: HttpRequest<unknown>, next: HttpHandlerFn): Observable<HttpEvent<unknown>> {
+  const errorService = inject(ErrorService);
+  if (req.url.includes('/name')) {
+    return next(req).pipe(
+      catchError((err: HttpErrorResponse) => {
+        errorService.setCountryNotFoundApiError();
+        throw err;
+      })
+    );
+  }
+  return next(req);
+}
